46ab2c62a6d6dfda2940be984740368f501e607f
AntoineLODC <collot_a@esme.fr>
2018-01-12 13:23:04 +0100

    Created agent

--- Assets/Network.cs
+++ Assets/Network.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace NeuralNetwork
 {
     public class Network : MonoBehaviour
     {
-        InputLayer inputLayer;
+        [Header("Input Layer")]
 
-        NeuronLayer[] hiddenLayers;
+        public InputLayer inputLayer;
 
-        OutputLayer outputLayer;
+        [Header("Hidden Layers")]
+
+        public NeuronLayer[] hiddenLayers;
+
+        [Header("Output Layer")]
+
+        public NeuronLayer outputLayer;
+
+        [System.Serializable]
+        public class OutputEvent : UnityEvent<bool> { }
+        public OutputEvent onOutput = new OutputEvent();
 
         // Use this for initialization
         void Start()
@@ -23,10 +34,94 @@ namespace NeuralNetwork
                 connectionsCount = hiddenLayers[i].neuronCount;
             }
 
-            //Initialize output
+            outputLayer.InitializeNeurons(connectionsCount);
+            connectionsCount = outputLayer.neuronCount;
         }
 
-        
+        [ContextMenu("Compute")]
+        public void Compute()
+        {
+            float[] inputs = inputLayer.inputs;
+
+            for (int i = 0; i < hiddenLayers.Length; i++)
+            {
+                inputs = hiddenLayers[i].Compute(inputs);
+            }
+
+            if (outputLayer.Compute(inputs)[0] >= 0.5f)
+                onOutput.Invoke(true);
+            else
+                onOutput.Invoke(false);
+        }
     }
 
+    public class InputLayer : MonoBehaviour
+    {
+        public float[] inputs;
+    }
+
+    [System.Serializable]
+    public struct NeuronLayer
+    {
+        public int neuronCount;
+
+        Neuron[] neurons;
+
+        public float[] Compute(float[] inputs)
+        {
+            float[] outputs = new float[neurons.Length];
+
+            for (int i = 0; i < neurons.Length; i++)
+            {
+                outputs[i] = neurons[i].Compute(inputs);
+            }
+
+            return outputs;
+        }
+
+        public void InitializeNeurons(int connectionsCount)
+        {
+            neurons = new Neuron[neuronCount];
+
+            for (int i = 0; i < neurons.Length; i++)
+            {
+                neurons[i] = new Neuron(connectionsCount);
+            }
+        }
+    }
+
+    public struct Neuron
+    {
+        public Neuron(int inputCount)
+        {
+            weights = new float[inputCount];
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = Random.Range(-1, 1);
+            }
+            activationValue = Random.Range(0, 1);
+        }
+
+        public float Compute(float[] inputs)
+        {
+            float output = 0;
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                output += inputs[i] * weights[i];
+            }
+
+            if (output >= activationValue)
+                output = 1;
+            else
+                output = 0;
+
+            return output;
+        }
+
+        public float[] weights;
+
+        public int activationValue;
+    }
 }
---
+++ Assets/RaycastInput.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NeuralNetwork;
+
+public class RaycastInput : InputLayer {
+
+    [Range(2,50)]
+    [SerializeField]
+    int raycastCount;
+
+    [SerializeField]
+    float raycastAngleRange;
+
+    [SerializeField]
+    float raycastMaxDistance;
+
+    [SerializeField]
+    LayerMask raycastLayer;
+
+    Vector3[] directions;
+
+	// Use this for initialization
+	void Awake () {
+        CalculateDirections();
+        inputs = new float[raycastCount];
+    }
+
+    void CalculateDirections()
+    {
+        directions = new Vector3[raycastCount];
+
+        Vector3 startDirection = Vector3.right;
+        startDirection = Quaternion.AngleAxis(-raycastAngleRange * 0.5f, Vector3.forward) * startDirection;
+
+        float deltaAngle = raycastAngleRange / (raycastCount-1);
+        Vector3 currentDirection = startDirection;
+
+        for (int i = 0; i < directions.Length; i++)
+        {
+            directions[i] = currentDirection;
+            currentDirection = Quaternion.AngleAxis(deltaAngle, Vector3.forward) * currentDirection;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < directions.Length; i++)
+        {
+            float hitDistance = Physics2D.Raycast(transform.position, directions[i], raycastMaxDistance, raycastLayer).distance;
+            inputs[i] = hitDistance / raycastMaxDistance;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        CalculateDirections();
+
+        foreach (Vector3 dir in directions)
+                Gizmos.DrawLine(transform.position, transform.position + dir * raycastMaxDistance);
+    }
+}
